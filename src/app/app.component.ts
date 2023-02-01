d7b333330c400a4d0e067568c831dfd303033d38
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-02-01 02:50:15 -0300

    feat:cep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { CepServiceService } from './cep-service.service';
+import { ApiService } from './api.service';
+import { UserModel } from './user-model';
 
 export interface DadosCep {
   cep: string;
@@ -16,19 +18,24 @@ export interface DadosCep {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'gew';
+  name = '';
   email = new FormControl('');
+  cep = new FormControl('');
   hide = true;
+  api: any;
 
   getErrorMessage() {
     return this.email.hasError('required')
-      ? 'You must enter a value'
+      ? 'Coloque um endereço de e-mail válido!'
       : this.email.hasError('email')
-      ? 'Not a valid email'
+      ? 'Coloque um endereço de e-mail válido'
       : '';
   }
 
-  constructor(private cepService: CepServiceService) {}
+  constructor(
+    private cepService: CepServiceService,
+    private apiService: ApiService
+  ) {}
 
   consultaCep(valor: string, form: any) {
     this.cepService
@@ -48,5 +55,21 @@ export class AppComponent {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     return filterValue;
+  }
+
+  onSubmit(form: any) {
+    const user: UserModel = {
+      name: form.value.name,
+      email: form.value.email,
+      birth: form.value.birth,
+      cep: form.value.cep,
+      address: form.value.logradouro,
+      city: form.value.cidade,
+      state: form.value.estado,
+      password: form.value.password,
+      confirmPassword: form.value.confirmPassword,
+    };
+
+    this.apiService.save(user).subscribe((result) => {});
   }
 }
---
+++ src/app/user-model.ts
@@ -0,0 +1,11 @@
+export interface UserModel {
+  name: string;
+  email: string;
+  birth: string;
+  cep: string;
+  address: string;
+  city: string;
+  state: string;
+  password: string;
+  confirmPassword: string;
+}
